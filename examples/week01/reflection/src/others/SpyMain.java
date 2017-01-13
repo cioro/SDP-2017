40a2f58b39f9c5a27679768880c8f46891bcbfa9
Molina Atienza, Rose <rmolina16@bloomberg.com>
2017-01-13 19:03:09 +0000

    Fixed broken SpyMain - it couldn't find the class since the name was not complete

--- examples/week01/reflection/src/others/SpyMain.java
+++ examples/week01/reflection/src/others/SpyMain.java
@@ -4,7 +4,7 @@ package others;
 public class SpyMain {
     public static void main(String args[]) {
         try {
-            Spy thisClassPrinter = Spy.getSpy("Spy");
+            Spy thisClassPrinter = Spy.getSpy("others.Spy");
             thisClassPrinter.printAll(args);
         } catch (ClassNotFoundException ex) {
             System.err.println("It broke!");
